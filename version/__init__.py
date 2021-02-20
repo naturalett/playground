9c28e0322cb3988db334a15a87fce280840c40dc
lidor ettinger <lidor.ettinger@gmail.com>
2021-02-21 00:36:30 +0200

    fix service range map

--- version/__init__.py
+++ version/__init__.py
@@ -7,6 +7,6 @@ of the semver specifictation
 """
 
 #: Semver version
-__version__ = "0.0.1rc5-dev1"
+__version__ = "0.0.1rc5-dev2"
 #: Sha hash
 __sha__ = "b949a119010dc604c4ee1bc4c5ca303a8b145ab3"
